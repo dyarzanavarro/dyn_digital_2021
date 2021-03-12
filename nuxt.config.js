bea0f81f6e35557c620971ea9cf1ccd6d0068b75
Dan <danielyarza1@gmail.com>
2021-03-12 11:40:19 +0100

    firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,16 +46,16 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          apiKey: '<AIzaSyBe6RRGe8mAbCXsApOGyqnFHVWSBaqmrPE>',
-          authDomain: '<dyn-digital.firebaseapp.com>',
-          projectId: '<dyn-digital>',
-          storageBucket: '<dyn-digital.appspot.com>',
-          messagingySenderId: '<746324157668>',
-          appId: '<1:746324157668:web:ab3da0266442721a5238f1>',
-          measurementId: '<G-THRCLJE0G7>'
+          apiKey: "AIzaSyBe6RRGe8mAbCXsApOGyqnFHVWSBaqmrPE",
+          authDomain: "dyn-digital.firebaseapp.com",
+          databaseURL: "https://dyn-digital.firebaseio.com",
+          projectId: "dyn-digital",
+          storageBucket: "dyn-digital.appspot.com",
+          messagingSenderId: "746324157668",
+          appId: "1:746324157668:web:ab3da0266442721a5238f1",
+          measurementId: "G-THRCLJE0G7"
         },
         services: {
-          auth: true // Just as example. Can be any other service.
         }
       }
     ]
